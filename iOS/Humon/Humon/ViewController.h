7ce68b80a0366cf49af6a4117336a042644bc59a
Charles Massry <charlesdmassry@gmail.com>
2015-05-04 20:21:59 -0400

    initialized iOS APP

---
+++ iOS/Humon/Humon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Humon
+//
+//  Created by Charlie Massry on 5/4/15.
+//  Copyright (c) 2015 Charlie Massry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
